204c4cf6f08cac825d2cbfa89ea5477d21d1f451
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-18 19:32:57 -0300

    feat: update base form' ~

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,35 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const [output, setOutput] = useState('')
+  const { register, handleSubmit } = useForm()
+
+  function createUser(data: any) {
+    console.log(data)
+    setOutput(JSON.stringify(data, null, 2))
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <form action="" className="flex flex-col gap-4 w-full max-w-xs">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-10">
+      <form action="" className="flex flex-col gap-4 w-full max-w-xs" onSubmit={handleSubmit(createUser)}>
 
         <div className="flex flex-col gap-4">
           <label htmlFor="">E-mail</label>
-          <input type="email" name="email" className="border-zinc-300 shadow-sm rounded h-10 px-3" />
+          <input type="email" className="border-zinc-400 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white"{...register('email')} />
         </div>
 
         <div className="flex flex-col gap-4">
           <label htmlFor="">Senha</label>
-          <input type="password" name="password" className="border-zinc-300 shadow-sm rounded h-10 px-3" />
+          <input type="password" className="border-zinc-400 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white" {...register('password')} />
         </div>
 
         <button type="submit" className="bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-700">Salvar</button>
       </form>
+      <pre>
+        {output}
+      </pre>
     </main>
   );
 }
