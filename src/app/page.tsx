1201a5e38412749c5dfbd538947ff1d4f93f105b
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-03-20 19:11:24 -0300

    feat: validacao de campos e map array de inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,8 @@ const createUserFormSchema = z.object({
   password: z.string().min(6, 'pelomenos 6 caracteres ne meu nobre'),
   techs: z.array(z.object({
     title: z.string().min(1, 'nao pode ficar vazio'),
-    knowledge: z.number().min(1).max(100)
-  }))
+    knowledge: z.coerce.number().min(1, 'precisa de algo').max(100)
+  })).min(2, 'Pelomenos 2 tech ne patrao que isso')
 
 })
 // schema nada mais e doque uma representacao de uma estrutura de dados
@@ -61,33 +61,49 @@ export default function Home() {
         <div className="flex flex-col gap-4">
           <label htmlFor="">Nome</label>
           <input type="text" className="border-zinc-400 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white"{...register('name')} />
-          {errors.name && <span>{errors.name.message}</span>}
+          {errors.name && <span className="text-red-500">{errors.name.message}</span>}
         </div>
+
 
         <div className="flex flex-col gap-4">
           <label htmlFor="">E-mail</label>
           <input type="email" className="border-zinc-400 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white"{...register('email')} />
-          {errors.email && <span>{errors.email.message}</span>}
+          {errors.email && <span className="text-red-500">{errors.email.message}</span>}
         </div>
 
         <div className="flex flex-col gap-4">
+          <label htmlFor="">Password</label>
+          <input type="password" className="border-zinc-400 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white"{...register('password')} />
+          {errors.password && <span className="text-red-500">{errors.password.message}</span>}
+        </div>
+        <div className="flex flex-col gap-4">
           <label htmlFor="" className="flex items-center justify-between">Tecnologias</label>
-          <button onClick={addNewTech} className="">Adicionar</button>
+          <button onClick={addNewTech} type="button" className="text-emerald-600 text-sm">Adicionar</button>
           {
             fields.map((field, index) => {
               return (
-                <div key={field.id}>
-                  <input type="text"
-                    className="border-zinc-400 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white"
-                    {...register(`techs.${index}.title`)} />
+                <div key={field.id} className="flex gap-2">
+                  <div>
 
-                  <input type="number"
-                    className="border-zinc-400 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white"
-                    {...register(`techs.${index}.knowledge`)} />
+                    <input type="text"
+                      className="border-zinc-400 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white flex-1"
+                      {...register(`techs.${index}.title`)} />
+                    {errors.techs?.[index]?.title && <span className="text-red-500">{errors.techs?.[index]?.title?.message}</span>}
+                  </div>
+
+                  <div>
+                    <input type="number"
+                      className="border-zinc-400 shadow-sm rounded h-10 px-3 bg-zinc-800 text-white w-16 flex-1"
+                      {...register(`techs.${index}.knowledge`)} />
+                    {errors.techs?.[index]?.knowledge && <span className="text-red-500" >{errors.techs?.[index]?.knowledge?.message}</span>}
+                  </div>
                 </div>
+
               )
             })
           }
+
+          {errors.techs && <span className="text-red-500">{errors.techs.message}</span>}
         </div>
         <button type="submit" className="bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-700">Salvar</button>
       </form>
